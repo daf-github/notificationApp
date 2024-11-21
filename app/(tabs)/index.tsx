52cb27e03b55138f85273c44c5f599537803c1ab
Daniel Alejandro Fernández <83842237+globalesur-df@users.noreply.github.com>
2024-11-21 06:30:54 -0300

    After first notification

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,40 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { View } from '@/components/Themed';
+import { useEffect } from 'react';
+
+import * as Notifications from 'expo-notifications';
 
 export default function TabOneScreen() {
+  useEffect(() => {
+    Notifications.requestPermissionsAsync({
+      ios: {
+        allowAlert: true,
+        allowBadge: true,
+        allowSound: true,
+      },
+    }).then((status) => console.log('PERM:', JSON.stringify(status, null, 4)));
+  }, []);
+
+  const scheduleNotifications = async () => {
+    console.log('scheduleNotifications function');
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Open the notification to read them all',
+        sound: 'email_sound.wav', // <- for Android below 8.0
+      },
+      trigger: {
+        seconds: 2,
+        channelId: 'new_emails', // <- for Android 8.0+, see definition above
+      },
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Schedule" onPress={scheduleNotifications} />
     </View>
   );
 }
